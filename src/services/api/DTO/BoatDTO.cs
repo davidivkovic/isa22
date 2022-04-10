2ec55ca0fff416a3512967bc665f7c466b3697b6
MMomcilovic <61472289+MMomcilovic@users.noreply.github.com>
2022-04-11 01:50:58 +0200

    Updated BoatDTOs
    #6 #9

--- src/services/api/DTO/BoatDTO.cs
+++ src/services/api/DTO/BoatDTO.cs
@@ -1,33 +1,20 @@
-﻿namespace API.DTO;
+﻿using API.Core.Model;
+
+namespace API.DTO;
 
 public class CreateBoatDTO : CreateBusinessDTO
 {
-    public string BoatType { get; set; }
-    public string BoatLength { get; set; }
-    public string MotorNumber { get; set; }
-    public double MotorPower { get; set; }
-    public double MaxSpeed { get; set; }
-    public List<string> NavigationEquipment { get; set; }
-    public List<string> FishingEquipment { get; set; }
+    public BoatCharacteristics Characteristics { get; set; }
+    public BoatEquipment Equipment { get; set; }
 }
 public class UpdateBoatDTO : UpdateBusinessDTO
 {
-    public string BoatType { get; set; }
-    public string BoatLength { get; set; }
-    public string MotorNumber { get; set; }
-    public double MotorPower { get; set; }
-    public double MaxSpeed { get; set; }
-    public List<string> NavigationEquipment { get; set; }
-    public List<string> FishingEquipment { get; set; }
+    public BoatCharacteristics Characteristics { get; set; }
+    public BoatEquipment Equipment { get; set; }
 }
 public class BoatDTO : BusinessDTO
 {
-    public string BoatType { get; set; }
-    public string BoatLength { get; set; }
-    public string MotorNumber { get; set; }
-    public double MotorPower { get; set; }
-    public double MaxSpeed { get; set; }
-    public List<string> NavigationEquipment { get; set; }
-    public List<string> FishingEquipment { get; set; }
+    public BoatCharacteristics Characteristics { get; set; }
+    public BoatEquipment Equipment { get; set; }
 }
 
