16e5a890229094113081a03d30c0939e407c2441
milicad00 <draca.milica10@gmail.com>
2022-04-10 18:02:10 +0200

    Join registration front and back

---
+++ src/web/spa/src/api/auth.js
@@ -0,0 +1,43 @@
+import { setUser, removeUser } from '../stores/userStore.js'
+import { instance, fetch, setAccesToken, removeAccesToken } from './http.js'
+
+const signIn = data => {
+  return fetch(instance.post('auth/email/sign-in', data), data => {
+    console.log(data)
+    // window.location.href = '/'
+    setAccesToken(data.accessToken)
+    setUser(data.user)
+  })
+}
+
+const signOut = async () => {
+  await instance.post('auth/sign-out', {})
+  removeUser()
+  removeAccesToken()
+  window.location.href = '/'
+}
+
+const register = data => {
+  return fetch(instance.post('auth/email/sign-up', data))
+}
+
+const confirmEmail = (email, code) => {
+  return fetch(instance.post('auth/email/confirm', { email, code }))
+}
+
+const sendConfirmation = email => {
+  return fetch(instance.post('auth/email/send-confirmation', email))
+}
+
+const acceptRegistration = (email, denialReason) => {
+  return fetch(instance.post('auth/registration/accept', email))
+}
+
+export default {
+  signIn,
+  signOut,
+  register,
+  confirmEmail,
+  sendConfirmation,
+  acceptRegistration
+}
