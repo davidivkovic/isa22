845cbd587f4888a5a8f16431427f64068cd2a3c0
milicad00 <draca.milica10@gmail.com>
2022-04-10 17:55:38 +0200

    Create a sign in form
    Create header, sign in form and deleted unnecessary files

---
+++ src/web/spa/src/components/TheHeader.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="w-full">
+    <div class="xl mx-auto max-w-4.5xl">
+      <div class="flex items-center justify-between py-6">
+        <div>
+          <div class="flex items-center space-x-8">
+            <HeaderNavigation
+              :isActive="selectedTab === 0"
+              name="Cabins"
+              routeName="cabins"
+            >
+              <TreesIcon class="-mt-1 h-6 w-6" />
+            </HeaderNavigation>
+            <HeaderNavigation
+              :isActive="selectedTab === 1"
+              name="Boats"
+              routeName="boats"
+            >
+              <SailboatIcon class="h-6 w-6" />
+            </HeaderNavigation>
+            <HeaderNavigation
+              :isActive="selectedTab === 2"
+              name="Fishing"
+              routeName="fishing"
+            >
+              <FishIcon class="h-6 w-6" />
+            </HeaderNavigation>
+          </div>
+          <div
+            class="h-1 w-[5.1rem] rounded-3xl bg-emerald-600 transition duration-200 ease-in-out"
+            :class="translation"
+          ></div>
+        </div>
+        <div class="-ml-12 text-2xl font-medium tracking-tight">
+          adventure.com
+        </div>
+        <div class="flex items-center space-x-3">
+          <Button class="bg-emerald-600 text-white">
+            List your business
+          </Button>
+          <RouterLink v-if="!isAuthenticated" to="/signin">
+            <Button class="border border-gray-300">Sign in</Button>
+          </RouterLink>
+          <Button v-else @click="signOut()" class="border border-gray-300"
+            >Sign out</Button
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { useRoute, RouterLink } from 'vue-router'
+import HeaderNavigation from './HeaderNavigation.vue'
+import { TreesIcon, SailboatIcon, FishIcon } from 'vue-tabler-icons'
+import Button from './ui/Button.vue'
+import api from '../api/api'
+
+import { isAuthenticated } from '../stores/userStore.js'
+
+const currentRoute = computed(() => useRoute().name)
+const defaultTransition = 'translate-x-0'
+
+const signOut = () => api.auth.signOut()
+
+const selectedTab = computed(() => {
+  if ('cabins' === currentRoute.value) return 0
+  else if ('boats' === currentRoute.value) return 1
+  else if ('fishing' === currentRoute.value) return 2
+  else if (selectedTab.value) return selectedTab.value
+  return 0
+})
+
+const translation = computed(() => {
+  if (selectedTab.value === 0) return 'translate-x-0'
+  else if (selectedTab.value === 1) return 'translate-x-[105px]'
+  else if (selectedTab.value === 2) return 'translate-x-[212px]'
+  else if (translation.value) return translation.value
+  return defaultTransition
+})
+</script>
---
+++ src/web/spa/src/components/ui/Input.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="w-full">
+    <label
+      v-if="$attrs.label"
+      :for="$attrs.id"
+      class="mb-1 block pl-px text-[13.5px] text-neutral-700"
+    >
+      {{ $attrs.label }}
+    </label>
+    <div class="relative">
+      <div
+        ref="prepend"
+        v-if="$slots.prepend"
+        class="absolute left-0 flex h-full items-center pb-0.5"
+      >
+        <slot name="prepend" :focused="focused" :hovered="hovered"></slot>
+      </div>
+      <input
+        ref="input"
+        type="text"
+        spellcheck="false"
+        v-bind="$attrs"
+        :value="inputValue"
+        :style="inputStyle"
+        :name="name"
+        @focusin="focused = true"
+        @focusout="focused = false"
+        @mouseover="hovered = true"
+        @mouseleave="hovered = false"
+        @input="e => onInput(e)"
+        class="hover:disabled:border-neutral-30 0 block rounded-md border-neutral-300 px-2.5 py-2.5 text-sm placeholder-neutral-400 transition-colors hover:border-neutral-500 focus:border-neutral-500 focus:ring-0 focus:ring-transparent disabled:text-neutral-300 focus:disabled:border-neutral-300"
+      />
+      <div
+        ref="append"
+        class="absolute inset-y-0 right-0 flex h-full items-center"
+      >
+        <div
+          @click="clearInput()"
+          v-if="(clearable || clearable == '') && !!inputValue"
+          class="m-1 cursor-pointer p-1 text-neutral-400 hover:text-gray-700"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-[18px] w-[18px]"
+            viewBox="0 0 24 24"
+            stroke-width="2"
+            stroke="currentColor"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <line x1="18" y1="6" x2="6" y2="18"></line>
+            <line x1="6" y1="6" x2="18" y2="18"></line>
+          </svg>
+        </div>
+        <slot
+          name="append"
+          v-else-if="$slots.append"
+          :input="input"
+          :focused="focused"
+          :hovered="hovered"
+        ></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const props = defineProps(['modelValue', 'clearable', 'name'])
+const emit = defineEmits(['update:modelValue'])
+
+const input = ref()
+const prepend = ref()
+const append = ref()
+const inputStyle = ref('')
+const inputValue = ref(props.modelValue)
+const focused = ref(false)
+const hovered = ref(false)
+
+const onInput = event => {
+  inputValue.value = event.target.value
+  emit('update:modelValue', inputValue.value)
+}
+
+const clearInput = () => {
+  inputValue.value = ''
+  emit('update:modelValue', '')
+}
+
+onMounted(() => {
+  inputStyle.value = `
+    padding-left: ${prepend.value?.clientWidth - 4}px;
+    padding-right: ${append.value?.clientWidth - 4}px;
+  `
+})
+</script>
+
+<script>
+export default {
+  inheritAttrs: false
+}
+</script>
