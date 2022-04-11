82572a5333c7f94f345394d2148897268b17e2f3
MMomcilovic <61472289+MMomcilovic@users.noreply.github.com>
2022-04-11 02:04:51 +0200

    Implemented DTO and endpoints for cabin creation
    #8

---
+++ src/services/api/DTO/CabinDTO.cs
@@ -0,0 +1,17 @@
+﻿using API.Core.Model;
+
+namespace API.DTO;
+
+public class CreateCabinDTO : CreateBusinessDTO
+{
+    public List<Room> Rooms { get; set; }
+}
+public class UpdateCabinDTO : UpdateBusinessDTO
+{
+    public List<Room> Rooms { get; set; }
+}
+public class CabinDTO : BusinessDTO
+{
+    public List<Room> Rooms { get; set; }
+}
+
