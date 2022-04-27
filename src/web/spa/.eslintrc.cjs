3527bfb83ec26e95f2510e89259bd311d71a0ad0
David Ivkovic <gorajkora@gmail.com>
2022-04-27 07:15:44 +0200

    Fix eslint

--- src/web/spa/.eslintrc.cjs
+++ src/web/spa/.eslintrc.cjs
@@ -1,14 +1,27 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
-  "root": true,
-  "extends": [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-prettier"
-  ],
-  "env": {
-    "vue/setup-compiler-macros": true
+  root: true,
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended'],
+  env: {
+    'vue/setup-compiler-macros': true,
+    node: true
+  },
+  plugins: ['prefer-arrow'],
+  rules: {
+    semi: ['error', 'never'],
+    quotes: ['error', 'single'],
+    'comma-dangle': ['error', 'never'],
+    'prefer-arrow/prefer-arrow-functions': [
+      'error',
+      {
+        disallowPrototype: true,
+        singleReturnOnly: false,
+        classPropertiesAllowed: false
+      }
+    ],
+    'prefer-arrow-callback': ['error'],
+    'vue/multi-word-component-names': ['off']
   }
 }
