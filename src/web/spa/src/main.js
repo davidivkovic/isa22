f61cb85065c1288e631590a83ef5199271cff8c4
David Ivkovic <gorajkora@gmail.com>
2022-05-25 01:11:24 +0200

    Update main.js

--- src/web/spa/src/main.js
+++ src/web/spa/src/main.js
@@ -4,11 +4,6 @@ import App from './App.vue'
 import router from './router'
 import './index.css'
 
-Date.prototype.addDays = function (days) {
-  this.setDate(this.getDate() + parseInt(days))
-  return this
-}
-
 const app = createApp(App)
 
 app.use(router)
